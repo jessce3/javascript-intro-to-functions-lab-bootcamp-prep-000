3e534f574e42993232349c7ba21d2a6207fa14cd
Jessica Chan <jessce3@gmail.com>
2017-10-10 02:54:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
   } else if (string === string.toUpperCase()) {
       return "YES INDEED!"
   } else if (string === "I love you, Grandma") {
-      return (string === "I love you, too")
+      return (string = "I love you, too")
   }
 }
