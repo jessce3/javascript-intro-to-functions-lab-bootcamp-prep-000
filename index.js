8088b4fc8e1958445559d77c09f3ca9fce53f968
Jessica Chan <jessce3@gmail.com>
2017-10-10 03:02:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-var sayHiToGrandma === "string"
+var sayHiToGrandma = "string"
 
 function sayHiToGrandma(string) {
 	if (string === string.toLowerCase()) {
